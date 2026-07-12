c286881b05142834038635c3846119cc9c1c4697
melyoda <amv123doom@gmail.com>
2026-07-12 14:11:29 +0300

    some updates

--- RefugeCampDashboard/app/Database/Migrations/2026-07-11-000001_CreateActivitiesTable.php
+++ RefugeCampDashboard/app/Database/Migrations/2026-07-11-000001_CreateActivitiesTable.php
@@ -51,6 +51,10 @@ class CreateActivitiesTable extends Migration
                 'type' => 'DATETIME',
                 'null' => true,
             ],
+            'deleted_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
         ]);
 
         $this->forge->addKey('id', true);
